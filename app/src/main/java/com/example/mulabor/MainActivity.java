031390f432edb705db8239e0cc8be9b6d6786144
pawelsoczowka <pawelsoczowka@gmail.com>
2019-04-05 23:05:26 +0200

    shape selector for highlighting buttons

--- app/src/main/java/com/example/mulabor/MainActivity.java
+++ app/src/main/java/com/example/mulabor/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
